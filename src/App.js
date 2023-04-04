c5f7582705cddab454544e554743f16555918561
Aryan Jalali <aryanjalali1234@gmail.com>
2023-04-04 15:30:53 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import * as React from "react";
+import "./App.css";
+import photo from "./assets/photo.svg";
+import onboardingDots from "./assets/onboardingDots.svg";
+import homeIndicator from "./assets/homeIndicator.svg";
+import Button from "react-bootstrap/Button";
+const App = () => {
+  const propsData = {
+    btnCreateAccount: {
+      size: "lg",
+      disabled: false,
+      active: true,
+      variant: "outline-dark",
+      children: "Create an account",
+    },
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="onboarding">
+      <img className="photo" src={photo} />
+      <span className="algorithm">Algorithm</span>
+      <span className="finding-love-through-music-one-s">
+        Finding love through music, one Spotify playlist at a time.
+      </span>
+      <img className="onboarding-dots" src={onboardingDots} />
+      <Button
+        className="btn-create-account-instance"
+        {...propsData.btnCreateAccount}
+      />
+      <span className="privacy-and-agreemen">
+        Already have an account? Sign In
+      </span>
+      <img className="home-indicator" src={homeIndicator} />
     </div>
   );
-}
-
+};
 export default App;
